46ea1c6511ef628a6980254326daf5935797de5c
smileling <liling2005521@163.com>
2017-03-19 21:56:10 +0800

    upload CombinationSum.java

--- src/algorithm/CombinationSum.java
+++ src/algorithm/CombinationSum.java
@@ -1,6 +1,7 @@
 package algorithm;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -37,14 +38,74 @@ A solution set is:
  */
 public class CombinationSum {
 
+	//sum 1
 	List<List<Integer>> ans = new ArrayList<List<Integer>>();
 	int[] cans = {};
 	
 	public List<List<Integer>> combinationSum(int[] candidates, int target) {
-		
+		this.cans = candidates;
+		Arrays.sort(cans);
+		backTracking(new ArrayList(), 0, target);
 		
 		return ans;
     }
+	
+	public void backTracking(List<Integer> cur, int from, int target){
+		if(target == 0) {
+			List<Integer> list = new ArrayList<Integer>(cur);
+			ans.add(list);
+		} else {
+			for (int i = from; i < cans.length && cans[i] <= target; i++){
+				cur.add(cans[i]);
+				backTracking(cur,i,target - cans[i]);
+				cur.remove(new Integer(cans[i]));
+			}
+		}
+	}
+	
+	//sum 2
+	List<List<Integer>> ans2 = new ArrayList<List<Integer>>();  
+    int[] num = {};  
+      
+    public List<List<Integer>> combinationSum2(int[] num, int target) {  
+        this.num = num;  
+        Arrays.sort(num);  
+        backTracking2(new ArrayList<Integer>(), 0, target);  
+        return ans2;  
+    }  
+      
+    public void backTracking2(List<Integer> cur, int from, int tar) {  
+        if (tar == 0) {  
+            //查看该解是否已经在结果集中，如对于输入[1,1]和1，只需放一个[1]到结果集中  
+            boolean exist = false;  
+            for (int i = ans2.size() - 1; i >= 0 ; i--) {  
+                List<Integer> tmp = ans2.get(i);  
+                if (tmp.size() != cur.size()) {  
+                    continue;  
+                }  
+                int j = 0;  
+                while (j < cur.size() && tmp.get(j) == cur.get(j)) {  
+                    j++;  
+                }  
+                if (j == cur.size()) {  
+                    exist = true;  
+                    break;  
+                }  
+            }  
+              
+            //如果当前解不在结果集中，把其加入到结果集中  
+            if (!exist) {  
+                List<Integer> list = new ArrayList<Integer>(cur);  
+                ans2.add(list);  
+            }  
+            return;  
+        }  
+        for (int i = from; i < num.length && num[i] <= tar; i++) {  
+            cur.add(num[i]);  
+            backTracking2(cur, i + 1, tar - num[i]);  
+            cur.remove(new Integer(num[i]));  
+        }  
+    }  
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
